cf09cee7055564153d49fe1d903246d231c58f5c
Nick Fausti <nick@smile-ml.com>
2022-06-24 02:44:53 -0400

    Add ability to toggle a class on multiple ids in an array.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function toggleClassOnID(classname, id) {
-
   const container = document.querySelector(`#${id}`);
   if (container.classList.contains(classname)) { 
-  	container.classList.remove('hidden');
+    container.classList.remove('hidden');
   }
   else {
-  	container.classList.add('hidden');
+    container.classList.add('hidden');
   }
+ }
+ 
+function toggleClassOnIDs(classname, ids) {
+  ids.forEach(id => toggleClassOnID(classname, id));
 }
